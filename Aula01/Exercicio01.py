924a3ca3837dad02ec9aa65251e6e12342996689
Beatriz Cavalcante <121120555+Beatriiz-Cavalcante@users.noreply.github.com>
2024-04-09 13:19:27 -0300

    Add files via upload

---
+++ Aula01/Exercicio01.py
@@ -0,0 +1,18 @@
+#operações matemáticas
+
+#pergunta02
+nota1 = float(input("Informe a nota 1:"))
+nota2 = float(input("Informe a nota 2:"))
+media = (nota1 + nota2)/2
+print(f'Sua primeira nota foi: {nota1}\nSua segunda nota foi:{nota2}\nLogo, sua média é:{media}\n\n\n')
+
+#pergunta03
+num1=float(input("Digite um número:"))
+num2=float(input("Digite outro número:"))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f'Numero 1: {num1}, Numero 2: {num2}\nSoma:{soma}\nSubtração:{subtracao}\nMultiplicação:{multiplicacao}')
