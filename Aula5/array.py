d20d9d7a715b6b26941a148a4dd03d2f21a4e8a0
Beatriz Cavalcante <121120555+Beatriiz-Cavalcante@users.noreply.github.com>
2024-04-17 17:02:17 -0300

    Listas

---
+++ Aula5/array.py
@@ -0,0 +1,9 @@
+Numeros = [1,4,3,9,100]
+
+print(Numeros[3])
+
+#para o caso de não saber o tamanho da lista e querer visualiza-la
+
+for x in range(len(Numeros)):
+    print(Numeros[x], end=" ") # o que está armazenado na lista
+    print(x, end=" ") #para retornar o índice
