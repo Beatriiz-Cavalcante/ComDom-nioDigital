72268bdb6b41b05cd9f9ce7e9e00f354ab6eea4d
Beatriiz_Cavalcante <biamenezes193@gmail.com>
2024-04-21 16:54:22 -0300

    rename

---
+++ Aula1/main.py
@@ -0,0 +1,8 @@
+#entendendo input e print
+nome = input("Digite o seu nome: ")
+idade = input("Digite a sua idade: ")
+cidade = input("Informe a sua cidade: ")
+print(f'Olá, {nome}!\nBem-vindo ao pyCharm')
+print(f'Você tem {idade} anos e é de {cidade}')
+
+
