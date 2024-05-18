b98f320f1df6331a0536d80cf7a5babd67fe6a2d
Beatriiz_Cavalcante <biamenezes193@gmail.com>
2024-05-18 11:42:14 -0300

    Funções sem métodos de funcionamento simultâneo

--- Aula9(Inicio POO)/classes.py
+++ Aula9(Inicio POO)/classes.py
@@ -7,36 +7,62 @@ classe tem a palabra reservada class + nome:
 constutores tem a palavra reservada __init__
 '''
 
+'''Fazer a classe pessoa de um modo que os métodos não possam ser chamados simultâneamente'''
+
 class Pessoa():
     def __init__(self, nome, peso, idade, comendo=False, dormindo=False, falando=False):
         self.nome = nome
         self.peso = peso
         self.idade = idade
-
+        self.comendo = comendo
+        self.dormindo = dormindo
+        self.falando = falando
+#ativar
     def comer(self, comida, bebida):
-        self.comendo = True
-        if self.comendo == True:
+        if self.dormindo == False and self.falando == False:
+            self.comendo = True
             print(f"{self.nome} foi comer {comida} e tomar {bebida}")
         elif self.dormindo == True:
-            print("Você não pode comer domindoo")
+             print("Você não pode comer domindoo")
         elif self.falando == True:
             print("Para de falar e depois coma!")
-        else:
-            print("Não estou comendo")
-
+            
     def dormir(self):
-        self.dormindo == True
-        if self.comendo == True:
+        if self.comendo == False and self.falando == False:
+            self.dormindo = True
+            print(f"{self.nome} está no maior ronco")
+        elif self.comendo == True:
             print("Você tem que parar de comer para dormir!")
         elif self.falando == True:
             print("Você tem que parar de falar para dormir")
-        else:
-            print(f"{self.nome} está no maior ronco")
+
     def falar(self):
-        self.falando == True
-        if self.comendo == True:
+        if self.comendo == False and self.dormindo == False:
+            self.falando = True
+            print(f"{self.nome} não gosta muito de falar.")
+        elif self.comendo == True:
             print("Eca, não fale de boca cheia!")
         elif self.dormindo == True:
-            print("Caraca, você é sonambulo")
+            print("Caraca, você é sonambulo??")
+
+#desativar
+    def pararDeComer(self):
+        if self.comendo == True:
+            print("Já to acabando")
+            self.comendo = False
         else:
-            print(f"{self.nome} não gosta muito de falar.")
+            print("Quê? Mas eu não estou comendo...")
+
+    def pararDeDormir(self):
+        if self.dormindo == True:
+            print("Calma, minha alma ta voltando para o corpo")
+            self.dormindo = True
+        else:
+            print("To acordado. Só to descansando a vista")
+
+    def pararDeFalar(self):
+        if self.falando == True:
+            print("Não ta mais aqui quem falou...")
+            self.falando = False
+        else:
+            print("QUê? Mas eu estou calado!")
--- Aula9(Inicio POO)/mainP.py
+++ Aula9(Inicio POO)/mainP.py
@@ -3,13 +3,14 @@ from classes import *
 p1 = Pessoa("Triz", 60, 22)
 p2 = Pessoa("Bea", 64, 22)
 
-print(p1.nome) #antes do ponto o nome do objeto e depois do ponto o atributo ou método a ser utilizado
+#print(p1.nome) antes do ponto o nome do objeto e depois do ponto o atributo ou método a ser utilizado
 print("Pessoa 1\n")
 p1.comer("Coxinha", "coquinha")
-p1.dormir()
+p1.pararDeComer()
 p1.falar()
+p1.dormir()
 
 print("\nPessoa 2\n")
-p2.comer("Uva", "água")
-p2.dormir()
 p2.falar()
+p2.dormir()
+p2.comer("Uva", "água")
