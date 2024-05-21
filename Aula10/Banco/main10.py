d30ada6b4bb23ca28555fd77d9bf267854be27f9
Beatriz Cavalcante <121120555+Beatriiz-Cavalcante@users.noreply.github.com>
2024-05-21 17:01:41 -0300

    trabalhando com herança

---
+++ Aula10/Banco/main10.py
@@ -0,0 +1,9 @@
+from classes10 import *
+
+conta1 = ContaBancaria(123, 'Triz', 'Conta Corrente')
+conta1.ativaConta()
+conta1.depositar(20)
+conta1.verificaSaldo()
+conta1.sacar(15)
+conta1.verificaSaldo()
+conta1.desativaConta()
---
+++ Aula10/heranca/mainH.py
@@ -0,0 +1,15 @@
+from classesH import *
+
+animal1 = Cachorro('Koda','brancodido')
+animal1.comer()
+animal1.latir()
+print()
+animal2 = Gato('Rocky', 'tricolor')
+animal2.comer()
+animal2.miar()
+print()
+animal3 = Coelho('Perna Longa', 'cinza')
+animal3.grunhir()
+print()
+animal4 = Vaca('mimosa', 'malhada')
+animal4.mugir()
